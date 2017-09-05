8dfc01dce604cdb5a8beb8584a909a207155591c
sameeJ <mamesjiles@gmail.com>
2017-09-05 15:26:20 +0100

    Index, 404, Generic

--- app/views.py
+++ app/views.py
@@ -1,25 +1,39 @@
+import os
+import tweepy
+
 from flask import render_template
 from app import app
+
+# Set up Twitter
+CON_KEY = 'hO1xjwg0YcI1YSQZBJBhbCGyG'
+CON_SEC = 'aPJlGid4V5Tl54TdZg0cJoGNGqaGFAxK9IPLlOn262DGWSrO23'
+ACC_TOK = '822896244200734720-Ettkj1QkZmYBHK0wSRMWvSeWyyykEje'
+ACC_SEC = 'RfpK24UGnCXs01rSTVoMYDv9FRMq0ATh0tyL5UBWUhQdv'
+
+auth = tweepy.OAuthHandler(CON_KEY, CON_SEC)
+auth.set_access_token(ACC_TOK, ACC_SEC)
+twitter = tweepy.API(auth)
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Miguel'}
-
     # Adding a comment for a git test
-
-    return render_template('index.html',
-                           user=user)
+    return render_template('index.html')
 
 
 @app.route('/DonaldTrump')
 def donald_trump():
+    latest_tweet = twitter.user_timeline(screen_name='realDonaldTrump', count=1)[0]
+
     return render_template('generic.html',
                            title='Donald Trump',
-                           info="Donald Trump is the current president of the USA.")
+                           currentCompanies=None,
+                           pastCompanies=None
+                           )
 
 
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('error404.html'), 404
+
