b829233c551767a904d2ec28b1f5b90b5f5d65e8
sameeJ <mamesjiles@gmail.com>
2017-09-08 01:12:27 +0100

    data passing to JS properly

--- app/views.py
+++ app/views.py
@@ -13,14 +13,18 @@ def index():
 @app.route('/DonaldTrump')
 def donald_trump():
     current_data = return_data('JohnFreakingSmi')
+    #past_data = return_past_data('realDonaldTrump')
 
     if len(current_data) == 0:
-        print("test")
         current_data = None
 
     return render_template('generic.html',
                            title='Donald Trump',
-                           data=json.dumps(current_data))
+                           info=f'Donald John Trump is the 45th and current President of the United States,'
+                                f' in office since January 20, 2017. Before entering politics,'
+                                f' he was a businessman and television personality.',
+                           data=json.dumps(current_data),
+                           pastdata=None)
 
 
 @app.errorhandler(404)
